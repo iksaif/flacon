aa7b3c394a5b3f7efef85bd86cc856f18ff49321
Corentin Chary <corentin.chary@gmail.com>
2022-06-20 07:29:38 +0000

    Bump to python 3.9 and bump all deps

--- setup.py
+++ setup.py
@@ -50,9 +50,9 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     # This makes sure the templates are easy to import.
     zip_safe=False,
